7ba049ad01ff09c261ee05c1794ac7ae6351fbf7
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2025-07-30 16:29:24 -0300

    :wrench: chore: add ESLint and Prettier configuration files

---
+++ eslint.config.js
@@ -0,0 +1,38 @@
+import js from '@eslint/js';
+import prettierConfig from 'eslint-config-prettier';
+import importPlugin from 'eslint-plugin-import';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  {
+    ignores: ['node_modules/', 'dist/', 'vitest.config.ts'],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.strict,
+  {
+    plugins: {
+      import: importPlugin,
+    },
+    rules: {
+      ...importPlugin.configs.recommended.rules,
+      'import/order': [
+        'error',
+        {
+          groups: ['builtin', 'external', 'internal', ['parent', 'sibling', 'index']],
+          'newlines-between': 'always',
+          alphabetize: { order: 'asc', caseInsensitive: true },
+        },
+      ],
+      'import/no-unresolved': 'off',
+    },
+  },
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parserOptions: {
+        project: true,
+      },
+    },
+  },
+  prettierConfig
+);
