b0b64c16ddad167454575e5beee5de0e67acde27
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2025-08-04 19:53:08 -0300

    ✨ feat: implement card number, security code, and expiration date generation in CardService

--- src/services/cardService.ts
+++ src/services/cardService.ts
@@ -1,26 +1,37 @@
+import Chance from 'chance';
+
 import * as cardRepository from '@/repositories/cardRepository.js';
 import { Company } from '@/repositories/companyRepository.js';
 
 export class CardService {
-  private generateUniqueCardNumber() {
-    // TODO: Implementar lógica para gerar número de cartão único
+  private readonly chance = new Chance();
+
+  generateUniqueCardNumber(): string {
+    const cardNumber = this.chance.cc();
+    return cardNumber;
   }
 
-  private generateSecurityCode() {
-    // TODO: Implementar lógica para gerar código de segurança
+  generateSecurityCode(): string {
+    const securityCode = this.chance.string({ length: 3, pool: '0123456789' });
+    return securityCode;
   }
 
-  private generateExpirationDate() {
-    // TODO: Implementar lógica para gerar data de expiração
+  generateExpirationDate(): string {
+    const actualDate = new Date();
+    const expirationDate = new Date(actualDate.setFullYear(actualDate.getFullYear() + 5));
+    const date = expirationDate.toISOString().split('T')[0];
+    const month = date.split('-')[1];
+    const year = date.split('-')[0].slice(-2);
+    return `${month}/${year}`;
   }
 
   async createCard(company: Company, employeeId: number, type: string) {
     const cardData = {
       employeeId,
-      number: '',
       cardholderName: '',
-      securityCode: '',
-      expirationDate: '',
+      number: this.generateUniqueCardNumber(),
+      securityCode: this.generateSecurityCode(),
+      expirationDate: this.generateExpirationDate(),
       isVirtual: false,
       isBlocked: false,
       type: type as cardRepository.TransactionTypes,
