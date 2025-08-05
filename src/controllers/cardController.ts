b0cef1fe0acd29094ec4b41ec89e661f9635cb21
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2025-08-04 22:28:22 -0300

    :recycle: refactor:  simplify card creation

--- src/controllers/cardController.ts
+++ src/controllers/cardController.ts
@@ -1,20 +1,15 @@
 import { Response } from 'express';
 
 import { AuthenticatedRequest } from '@/middlewares/authMiddleware.js';
-import { Company } from '@/repositories/companyRepository.js';
+import { TransactionTypes } from '@/repositories/cardRepository.js';
 import { cardService } from '@/services/cardService.js';
 
 class CardController {
   async createCard(req: AuthenticatedRequest, res: Response) {
-    try {
-      const { employeeId, type } = req.body;
-      const company = req.company as Company;
+    const { employeeId, type } = req.body;
 
-      await cardService.createCard(company, employeeId, type);
-      res.status(201).send({ message: 'Card created successfully' });
-    } catch (error) {
-      res.status(400).json({ error: (error as Error).message });
-    }
+    await cardService.createCard(employeeId, type as TransactionTypes);
+    res.status(201).send({ message: 'Card created successfully' });
   }
 }
 
