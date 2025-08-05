4e4ac26684d94c754e8bc01041e7bdfa34b084f1
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2025-08-05 02:07:37 -0300

    :test_tube: test: add database seeding and cleanup functions for testing environment

---
+++ tests/db/seed.sql
@@ -0,0 +1,20 @@
+TRUNCATE TABLE companies, employees, businesses RESTART IDENTITY CASCADE;
+
+INSERT INTO companies ("id", "name", "apiKey")
+VALUES (1, 'Driven', 'zadKLNx.DzvOVjQH01TumGl2urPjPQSxUbf67vs0');
+
+INSERT INTO employees ("id", "fullName", "cpf", "email", "companyId")
+VALUES (1, 'Fulano Rubens da Silva', '47100935741', 'fulano.silva@gmail.com', 1);
+INSERT INTO employees ("id", "fullName", "cpf", "email", "companyId")
+VALUES (2,'Ciclana Maria Madeira', '08434681895', 'ciclaninha@gmail.com', 1);
+
+INSERT INTO businesses ("id", "name", "type")
+VALUES (1, 'Responde Aí', 'education');
+INSERT INTO businesses ("id", "name", "type")
+VALUES (2, 'Extra', 'groceries');
+INSERT INTO businesses ("id", "name", "type")
+VALUES (3, 'Driven Eats', 'restaurant');
+INSERT INTO businesses ("id", "name", "type")
+VALUES (4, 'Uber', 'transport');
+INSERT INTO businesses ("id", "name", "type")
+VALUES (5, 'Unimed', 'health');
---
+++ tests/factories/scenarioFactory.ts
@@ -0,0 +1,13 @@
+import fs from 'fs';
+import path from 'path';
+
+import { connection } from '@/config/postgres.js';
+
+export async function seedDb() {
+  const seedQuery = fs.readFileSync(path.join(__dirname, '../db/seed.sql'), 'utf-8');
+  await connection.query(seedQuery);
+}
+
+export async function cleanDb() {
+  await connection.query('TRUNCATE TABLE cards, companies, employees RESTART IDENTITY CASCADE');
+}
