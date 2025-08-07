5050a5f4286395e04debdf409e20f1d122465f9e
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2025-08-07 14:07:52 -0300

    :recycle: refactor: replace generic error with AppError

--- src/services/companyService.ts
+++ src/services/companyService.ts
@@ -1,11 +1,11 @@
+import { AppError } from '@/errors/AppError.js';
 import { findByApiKey } from '@/repositories/companyRepository.js';
 
 class CompanyService {
   async getCompanyByApiKey(apiKey: string) {
     const company = await findByApiKey(apiKey);
-    if (!company) {
-      throw new Error('Company not found');
-    }
+    if (!company) throw new AppError('Company not found', 'not_found');
+
     return company;
   }
 }
