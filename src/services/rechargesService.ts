40b728415938463f9021f17e42d82740c88815a9
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2025-08-12 14:46:57 -0300

    ✨ feat: implement createRecharge method for handling recharge creation

--- src/services/rechargesService.ts
+++ src/services/rechargesService.ts
@@ -1,9 +1,18 @@
-import { findByCardId } from '@/repositories/rechargeRepository.js';
+import { AppError } from '@/errors/AppError.js';
+import { insert, findByCardId } from '@/repositories/rechargeRepository.js';
+import { cardService } from '@/services/cardService.js';
 
 export class RechargesService {
   async getRechargesByCardId(cardId: number) {
     const recharges = await findByCardId(cardId);
     return recharges;
+  }
+
+  async createRecharge(cardId: number, amount: number) {
+    const card = await cardService.findCardById(cardId);
+    if (!card.password) throw new AppError('Card is inactive', 'conflict');
+    await cardService.validateCardExpirationDate(card.expirationDate);
+    await insert({ cardId, amount });
   }
 }
 
