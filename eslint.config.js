c85348e5fd897252b9ceaeec763c4b18562470fd
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2025-08-12 14:45:19 -0300

    :wrench: chore: update ESLint ignore patterns

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   {
-    ignores: ['node_modules/', 'dist/', 'vitest.config.ts'],
+    ignores: ['node_modules/', 'dist/', 'vitest.config.*'],
   },
   js.configs.recommended,
   ...tseslint.configs.strict,
