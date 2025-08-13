0a69f18a5ebb334234de7d29b4b97aeea87bf49f
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2025-08-13 14:55:01 -0300

    🔧 chore: update ESLint configuration to use tsconfig.eslint.json for project settings

--- eslint.config.js
+++ eslint.config.js
@@ -30,7 +30,7 @@ export default tseslint.config(
     files: ['**/*.ts'],
     languageOptions: {
       parserOptions: {
-        project: true,
+        project: './tsconfig.eslint.json',
       },
     },
   },
